95ef11a0254d94c1cc260ec4cde617ad9efb0318
charlsraven <voron00100867@gmail.com>
2023-02-20 23:21:27 +0300

    menu + settings done

---
+++ app/src/main/java/my/tick_tac_toe/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package my.tick_tac_toe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
