2b536d8e7c584debce2e707a0d7875275719d83f
charlsraven <voron00100867@gmail.com>
2023-02-21 11:16:14 +0300

    0.49

--- app/src/main/java/my/tick_tac_toe/SettingsActivity.kt
+++ app/src/main/java/my/tick_tac_toe/SettingsActivity.kt
@@ -2,10 +2,18 @@ package my.tick_tac_toe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import my.tick_tac_toe.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
+    private var currentSound = 0
+    private var currentLvl = 0
+    private var currentRules = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
